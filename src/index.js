8dc9b642e1b719af74ebd082be4899a1761f7d96
121343789@qq.com <www.xxmi.cn>
2019-01-29 08:32:15 +0800

    工具类

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import Datetime from './datetime.js';
 import Image from './image.js';
+import Util from './util.js';
 import Tool from './tool.js';
 import Validator from './validator.js';
 
-export {Datetime, Image, Tool, Validator};
-export default {Datetime, Image, Tool, Validator};
+export {Datetime, Util, Image, Tool, Validator};
+export default {Datetime, Util, Image, Tool, Validator};
